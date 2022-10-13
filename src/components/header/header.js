538283d4871b198871d57ad527dc85ccb162c92d
Elena Ivanova <31884088+elenaplehanova@users.noreply.github.com>
2022-10-13 14:53:52 +0700

    #8 - Have been added semantic tags

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,9 +2,9 @@ import "./header.scss";
 
 const Header = () => {
     return (
-        <div className="header">
+        <header>
             <div className="header__title title text-primary-900">Your Adviser</div>
-        </div>
+        </header>
     );
 };
 
